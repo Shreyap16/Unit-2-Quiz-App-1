df318a7ff272d4f7fb361dc2b609cf5d76d5f924
Shreyap16 <spatel429@yahoo.com>
2017-04-17 03:02:44 -0400

    first commit

---
+++ quiz.ts
@@ -0,0 +1,11 @@
+var right = 0;
+var numOfQuestions = 5;
+var score = 0;
+function rightAnswer() {
+    console.log(`he got it right`);
+    score = score + 20;
+    right = right + 1;
+}
+function onSubmit() {
+  window.alert(`You got ${right} out of ${numOfQuestions} right , a score of ${score}`);
+}
